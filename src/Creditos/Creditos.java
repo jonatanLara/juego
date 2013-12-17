49ab160cdd1a8881f3345445eba7b2e750b91c91
usuario <usuario@192.168.0.8>
2013-12-16 20:35:05 -0600

    se agregaron las imagenes que faltaban en el frame vista2 y se terminaron las instrucciones, con audio. se agragaron imagenes al juego del gato. con audio.

--- src/Creditos/Creditos.java
+++ src/Creditos/Creditos.java
@@ -30,11 +30,15 @@ public class Creditos extends JLabel {
         //15 es mi separacion lateral y 90 es hacia abajo 
         g.drawString("Este juego fue creado por JK Jabakids S.R.L", 30, 60);
         g.drawString("DESARROLLADORES", 30, 110);
-        g.drawString("AREA ADMINISTRATIVA", 30, 130);
-        g.drawString("AREA DE MERCADOTECNIA", 30, 150);
-        g.drawString("AREA DE RECURSOS HUMANOS", 30, 170);
-        g.drawString("AREA DE INFORMATICA", 30, 190);
-        g.drawString("", 10, 210);
+        g.drawString("lara ortiz jonatan", 60, 130);
+        g.drawString("AREA ADMINISTRATIVA", 30, 150);
+        g.drawString("FIERROS MIJANGOS GREGORIA GUADALUPE", 60, 170);
+        g.drawString("AREA DE MERCADOTECNIA", 30, 190);
+        g.drawString("CHAN LÓPEZ SUNI ERI", 60, 210);
+        g.drawString("AREA DE RECURSOS HUMANOS", 30, 230);
+        g.drawString("RAMÍREZ GÓMEZ PETRONILO", 60, 250);
+        g.drawString("AREA DE INFORMATICA", 30, 270);
+        g.drawString("IXCAL PEDRO YOLANDA", 60, 290);
       /* g.setColor(Color.GRAY);
          g.drawString("tienes 10 oportunidades para ganar", 10, 230);
          g.drawString("s", 10, 250);
